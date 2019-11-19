47d6aba2719effb8ada4c28a06a188f42cbc52cf
Laurits Linderoth Lolck <lll2005@live.dk>
2019-11-19 18:47:56 +0100

    Added test_loops.py with while and for loops.

---
+++ test_loops.py
@@ -0,0 +1,22 @@
+string = "!nedrev jeH"
+
+# While loop
+string_laengde = len(string)
+current_position = string_laengde - 1
+while current_position >= 0:
+    print(string[current_position])
+    current_position -= 1
+
+# -----------------------------------------------------------------
+#string = "Hej verden!"
+#
+# While loop
+# string_laengde = len(string)
+# current_position = 0
+# while current_position < string_laengde:
+#     print(string[current_position])
+#     current_position += 1
+
+# For loop example
+# for char in string:
+#     print(char)
